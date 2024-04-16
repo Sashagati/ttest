030db44698ab1ed837e12fa315bed6a4585661e1
Alexandra Gatiyatullina <name>
2024-04-16 12:43:52 +0300

    тест на валидность изображения

--- laravel/tests/Feature/PostTest.php
+++ laravel/tests/Feature/PostTest.php
@@ -14,48 +14,48 @@ class PostTest extends TestCase
 {
 
     use RefreshDatabase;
-  /**
-   * @test
-   */
-  public function _post_can_be_stored()
-  {
+
+    /**
+     * @test
+     */
+    public function _post_can_be_stored()
+    {
 
 
-      $this->withoutExceptionHandling();
+        $this->withoutExceptionHandling();
 
-      Storage::fake('local');
-
-
-      $file = File::create('my_image.jpg');
+        Storage::fake('local');
 
 
-
-      $data = [
-          'title'=>'Some title',
-          'description'=>'description',
-          'image'=>$file,
+        $file = File::create('my_image.jpg');
 
 
-      ];
+        $data = [
+            'title' => 'Some title',
+            'description' => 'description',
+            'image' => $file,
 
 
+        ];
 
-      $res = $this->post('/posts',$data);
 
-      $res->assertStatus(200);
+        $res = $this->post('/posts', $data);
 
-      $this->assertDatabaseCount('posts',1);
+        $res->assertStatus(200);
 
-      $post = Post ::first();
+        $this->assertDatabaseCount('posts', 1);
 
-      $this->assertEquals($data['title'],$post->title);
-      $this->assertEquals($data['description'],$post->description);
+        $post = Post::first();
 
-      $this->assertEquals('images/' . $file->hashName(),$post->image_url);
+        $this->assertEquals($data['title'], $post->title);
+        $this->assertEquals($data['description'], $post->description);
 
-      Storage::disk('local')->assertExists($post->image_url);
+        $this->assertEquals('images/' . $file->hashName(), $post->image_url);
 
-  }
+        Storage::disk('local')->assertExists($post->image_url);
+
+    }
+
     /**
      * @test
      */
@@ -63,18 +63,38 @@ class PostTest extends TestCase
     {
 
         $data = [
-            'title'=>'',
-            'description'=>'description',
-            'image'=>''
+            'title' => '',
+            'description' => 'description',
+            'image' => ''
 
         ];
 
-        $res = $this->post('/posts',$data);
+        $res = $this->post('/posts', $data);
         $res->assertRedirect();
         $res->assertInvalid('title');
-
-
     }
 
+    /**
+     * @test
+     */
+    public function attribute_image_is_file_for_storing_post()
+    {
+
+        Storage::fake('local');
+
+
+        $file = File::create('my_image.jpg');
+
+        $data = [
+            'title' => 'Title',
+            'description' => 'description',
+            'image' => 'sdfsdf'
+
+        ];
+
+        $res = $this->post('/posts', $data);
+        $res->assertRedirect();
+        $res->assertInvalid('image');
+    }
 
 }
